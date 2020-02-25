7922c01656f9e10737df77ce101d54e537cb8c9e
Tristan de Lataillade <delataillade.tristan@gmail.com>
2020-02-24 23:33:08 -0500

    Tracis: fix bash syntax error

--- .travis/install.sh
+++ .travis/install.sh
@@ -7,8 +7,8 @@ if [[ "$1" == "conda" ]]; then
   then
     wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh;
   elif [[ "$2" == "osx" ]]
-    wget https://repo.continuum.io/miniconda/Miniconda3-latest-MacOSX-x86_64.sh -O miniconda.sh;
   then
+    wget https://repo.continuum.io/miniconda/Miniconda3-latest-MacOSX-x86_64.sh -O miniconda.sh;
   fi
   bash miniconda.sh -b -p $HOME/miniconda
   source "$HOME/miniconda/etc/profile.d/conda.sh"
