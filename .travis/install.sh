8a83d23437bb6e7bc1fb12cdbf6814c72bec82b8
Tristan de Lataillade <delataillade.tristan@gmail.com>
2020-02-24 23:22:27 -0500

    Travis: install script for linux and osx

--- .travis/install.sh
+++ .travis/install.sh
@@ -3,7 +3,13 @@
 if [[ "$1" == "conda" ]]; then
   git lfs pull
   sudo apt-get update
-  wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh;
+  if [[ "$2" == "linux" ]]
+  then
+    wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh;
+  elif [[ "$2" == "osx" ]]
+    wget https://repo.continuum.io/miniconda/Miniconda3-latest-MacOSX-x86_64.sh -O miniconda.sh;
+  then
+  fi
   bash miniconda.sh -b -p $HOME/miniconda
   source "$HOME/miniconda/etc/profile.d/conda.sh"
   hash -r
@@ -34,7 +40,7 @@ elif [[ "$1" == "hashdist" ]]; then
   export PATHSAVE=$PATH
   export PATH=$PWD/linux/bin:$PATH
   export LD_LIBRARY_PATH=$PWD/linux/lib:$LD_LIBRARY_PATH
-  PROTEUS_OPT="-w -O2 -UNDEBUG" FC=gfortran CC=mpicc CXX=mpicxx make develop N=2
+  PROTEUS_OPT="-w -O2 -UNDEBUG" FC=gfortran CC=mpicc CXX=mpicxx make develop N=1
   export SSL_CERT_DIR=/etc/ssl/certs
   #./linux/bin/pip3 install matplotlib
 fi
